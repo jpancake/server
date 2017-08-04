479b70d3f0bf92e95ab4914ed4852fb25db151a9
jpancake <jrazon_18@yahoo.com>
2017-08-04 13:00:37 -0700

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express()
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' })
-})
-
+// clientID 1046189866209-l5k3mcoh5jhansvapfv1i6d9dtbr2lhu.apps.googleusercontent.com
+// clientSecret b4PdmMIs2IxDYDSkraT9rwER
+passport.use(new GoogleStrategy())
 
 const PORT = process.env.PORT || 5000
 
